9d2d39eae79236eb97ec220cd25d672bae0f0ec6
Ted-Brink <hexasus@gmail.com>
2021-09-06 19:09:54 +0200

    Commit properties file for the test scope.

--- src/main/java/se/lexicon/firstspringbootproject/FirstSpringBootProjectApplication.java
+++ src/main/java/se/lexicon/firstspringbootproject/FirstSpringBootProjectApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstSpringBootProjectApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(FirstSpringBootProjectApplication.class, args);
 	}
 
